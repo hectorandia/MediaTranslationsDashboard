553d8194851aee5a01966448c350e33fc0b3f9bb
Hector Kong <otit1978@hotmail.com>
2017-06-30 13:21:27 +0200

    Add project files.

---
+++ DashboardMediaTranslations/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DashboardMediaTranslations.MvcApplication" Language="C#" %>
---
+++ DashboardMediaTranslations/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DashboardMediaTranslations.Startup))]
+namespace DashboardMediaTranslations
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
